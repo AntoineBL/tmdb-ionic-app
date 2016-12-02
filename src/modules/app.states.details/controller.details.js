66d6fb32ef25939f5f6a4629a76822a192414806
DESKTOP-OAANS63\Djypzi <antoineblanc269@laposte.net>
2016-12-02 14:46:19 +0100

    modif

---
+++ src/modules/app.states.details/controller.details.js
@@ -0,0 +1,17 @@
+/**
+ * @memberOf app.states.details
+ */
+(function (module) {
+  'use strict';
+
+  function DetailsController($scope) {
+    var controller = this;
+
+  }
+
+  module.controller('detailsController', [
+    '$scope',
+    DetailsController
+  ]);
+
+}(angular.module('app.states.details')));
--- src/modules/app.states.home/controller.home.js
+++ src/modules/app.states.home/controller.home.js
@@ -4,27 +4,13 @@
 (function (module) {
   'use strict';
 
-  function HomeController($scope, popupService, statesData) {
+  function HomeController($scope) {
     var controller = this;
 
-    $scope.statesData = statesData;
-
-    $scope.from = 'homeController';
-
-    controller.showPopup = function () {
-      var scope = { from: $scope.from, title: 'Popup' };
-      popupService.open(module, 'smartphone/popup', scope);
-    };
-
-    controller.onLanguageChange = function (locale) {
-      console.debug(locale);
-    };
   }
 
   module.controller('homeController', [
     '$scope',
-    'popupService',
-    'statesData',
     HomeController
   ]);
 
